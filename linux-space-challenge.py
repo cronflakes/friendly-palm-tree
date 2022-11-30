85a9fe3ef5a111441b4ebf2d5642478d0b379d3c
Matt Cassell <matt.cassell@tierpoint.com>
2022-11-30 12:26:35 -0600

    creating instance profile.
    resume on line 79

--- linux-space-challenge.py
+++ linux-space-challenge.py
@@ -5,12 +5,35 @@ def lambda_handler(event, context):
     # TODO implement
     db = boto3.client('dynamodb') 
     ec2 = boto3.client('ec2') 
-   
-    ec2_inst = ec2.start_instances(InstanceIds=['i-09ae6b7d1f332d4c6'],)
-    ec2_inst.wait_until_running()
+    iam = boto3.client('iam')
     
-    #this is failing because the ec2 instances may have a status of running, but the OS may not be fully booted (and the systemd service may not even be fully running either)
-    #check whether boto3 has an API for checking systemd services after lunch
+    inst_profile_name = 'linux-challenge-1-profile'
+    role_name = 'linux-challenge-1-role'
+    cleanIAMRolesAndProfiles(iam, role_name, inst_profile_name)
+    
+    policy = { 
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Principal": {
+                    "Service": "ec2.amazonaws.com"
+                },
+                "Action": "sts:AssumeRole"
+            }
+        ]
+    }
+   
+    inst_profile = iam.create_instance_profile(InstanceProfileName=inst_profile_name)
+    role = iam.create_role(RoleName=role_name, AssumeRolePolicyDocument=json.dumps(policy))
+    #iam.add_role_to_instance_profile(InstanceProfileName=inst_profile_name, RoleName=role_name)
+    iam.put_role_policy(RoleName='linux-challenge-1-role', PolicyName='linux-challenge-1-policy', PolicyDocument=policy)
+  
+    ec2_inst = ec2.run_instances(ImageId='ami-0abe861b63f2b08bb', InstanceType='t2.micro', MinCount=1, MaxCount=1, IamInstanceProfile={'Name': inst_profile_name})
+    inst_id = str(ec2_inst['Instances'][0]['InstanceId'])
+    waiter = ec2.get_waiter('instance_running')
+    waiter.wait(InstanceIds=[inst_id], Filters=[{ "Name":"instance-state-code", "Values": ["running"] }])
+    
     
     res = db.scan(TableName='linux-challenge')
     #print(res['Items'][0]['date']['S']) 
@@ -33,3 +56,24 @@ def getCredsFromDB(res):
             needed_query = i     
             
     return needed_query['password']['S']
+    
+
+def cleanIAMRolesAndProfiles(iam, role, profile):
+    try:
+        iam.delete_instance_profile(InstanceProfileName=profile)
+        print('deleted instance profile')
+    except iam.exceptions.NoSuchEntityException:
+        #instance profile doesn't exist
+        pass 
+    except iam.exceptions.DeleteConflictException as e:
+        print(e)
+    
+    try:
+        iam.delete_role(RoleName=role)
+        print('deleted role')
+    except iam.exceptions.NoSuchEntityException:
+        #role doesnt exist
+        pass 
+    except iam.exceptions.DeleteConflictException as e:
+        print(e)
+        #must remove role from instance profiles
